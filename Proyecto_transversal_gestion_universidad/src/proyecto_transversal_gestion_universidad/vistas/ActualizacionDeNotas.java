1a0b91f87a8e46dcb276783af30a2b1b9f639204
KevinPaico <141683583+KevinPaico@users.noreply.github.com>
2023-09-13 18:01:25 -0300

    refactorizar paquetes

--- Proyecto_transversal_gestion_universidad/src/proyecto_transversal_gestion_universidad/vistas/ActualizacionDeNotas.java
+++ Proyecto_transversal_gestion_universidad/src/proyecto_transversal_gestion_universidad/vistas/ActualizacionDeNotas.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package santiago;
+package proyecto_transversal_gestion_universidad.vistas;
 
 import java.beans.PropertyVetoException;
 import java.util.logging.Level;
