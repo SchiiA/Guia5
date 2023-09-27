6586b0d2b5f291d75f598434c30e07d69f325a29
KevinPaico <141683583+KevinPaico@users.noreply.github.com>
2023-09-27 20:10:32 -0300

    up

--- BD con datos/universidadulp.sql
+++ BD con datos/universidadulp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-09-2023 a las 00:33:33
+-- Tiempo de generación: 28-09-2023 a las 01:10:08
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -43,9 +43,16 @@ CREATE TABLE `alumno` (
 --
 
 INSERT INTO `alumno` (`idAlumno`, `dni`, `apellido`, `nombre`, `fechaNacimiento`, `estado`) VALUES
+(1, 42301733, 'Gonzáles', 'Marco', '1997-11-05', 1),
+(2, 42200748, 'Zannetti', 'Emmanuel', '2000-05-12', 1),
+(3, 4564, '', 'zxczxc', '2023-09-15', 1),
+(4, 12345, 'asd', 'qwe', '2023-09-20', 1),
+(5, 1234, 'perez', 'manuel', '2023-09-20', 1),
 (6, 43890576, 'Sanchez', 'Julián', '2001-02-28', 1),
-(8, 42301733, 'Gonzáles', 'Marco', '1997-11-05', 1),
-(9, 42200748, 'Zannetti', 'Emmanuel', '2000-05-12', 1);
+(7, 1256, 'flores', 'leandro', '2023-09-14', 1),
+(8, 1254, 'flores', 'andres', '2023-09-20', 1),
+(9, 123, 'perez', 'maria', '2023-09-14', 1),
+(10, 1123, 'asd', 'asd', '2023-09-15', 1);
 
 -- --------------------------------------------------------
 
@@ -55,10 +62,21 @@ INSERT INTO `alumno` (`idAlumno`, `dni`, `apellido`, `nombre`, `fechaNacimiento`
 
 CREATE TABLE `inscripcion` (
   `idInscripto` int(11) NOT NULL,
-  `nota` int(11) NOT NULL,
+  `nota` double DEFAULT NULL,
   `idAlumno` int(11) NOT NULL,
   `idMateria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `inscripcion`
+--
+
+INSERT INTO `inscripcion` (`idInscripto`, `nota`, `idAlumno`, `idMateria`) VALUES
+(1, 9, 9, 3),
+(2, 10, 9, 2),
+(3, 11, 10, 2),
+(4, 0, 6, 1),
+(5, 1, 9, 1);
 
 -- --------------------------------------------------------
 
@@ -78,10 +96,18 @@ CREATE TABLE `materia` (
 --
 
 INSERT INTO `materia` (`idMateria`, `nombre`, `año`, `estado`) VALUES
-(79, 'Derecho Constitucional', 1, 1),
-(215, 'Física', 1, 1),
-(568, 'Analisis Matemático I', 1, 1),
-(663, 'Nutrición y Regulación', 3, 1);
+(1, 'Derecho Constitucional', 1, 1),
+(2, 'Analisis Matemático I', 1, 1),
+(3, 'Física', 1, 1),
+(4, 'Física', 2, 1),
+(5, 'Física', 3, 1),
+(6, 'ingles', 3, 1),
+(7, 'ingles', 1, 1),
+(8, 'ingles', 2, 1),
+(9, 'matematica', 3, 1),
+(10, 'Matematica', 1, 1),
+(11, 'matematica', 2, 1),
+(12, 'Nutrición y Regulación', 3, 0);
 
 --
 -- Índices para tablas volcadas
@@ -107,7 +133,7 @@ ALTER TABLE `inscripcion`
 --
 ALTER TABLE `materia`
   ADD PRIMARY KEY (`idMateria`),
-  ADD UNIQUE KEY `nombre` (`nombre`);
+  ADD UNIQUE KEY `nombre_2` (`nombre`,`año`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -117,19 +143,19 @@ ALTER TABLE `materia`
 -- AUTO_INCREMENT de la tabla `alumno`
 --
 ALTER TABLE `alumno`
-  MODIFY `idAlumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idAlumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
 
 --
 -- AUTO_INCREMENT de la tabla `inscripcion`
 --
 ALTER TABLE `inscripcion`
-  MODIFY `idInscripto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idInscripto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `materia`
 --
 ALTER TABLE `materia`
-  MODIFY `idMateria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=664;
+  MODIFY `idMateria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=801;
 
 --
 -- Restricciones para tablas volcadas
